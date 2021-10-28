89ec433f4822e8fa7b89418dd1ec6d6408347a82
asmaabouta <asmaabouta99@gmail.com>
2021-10-29 00:32:06 +0200

    commit

---
+++ msbanque/src/main/java/com/asmaa/msbanque/MsbanqueApplication.java
@@ -0,0 +1,13 @@
+package com.asmaa.msbanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbanqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsbanqueApplication.class, args);
+    }
+
+}
---
+++ msbanque/src/test/java/com/asmaa/msbanque/MsbanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asmaa.msbanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbanqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
