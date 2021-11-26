95175bdf132ef642abf001c45afdc378af13e208
asmaabouta <asmaabouta99@gmail.com>
2021-11-26 20:09:38 +0100

    commit

--- gateway/src/main/java/com/asmaa/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/asmaa/gateway/GatewayApplication.java
@@ -2,6 +2,10 @@ package com.asmaa.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -9,5 +13,17 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+    //cette config va dire a spring cloud gateway que a chaque fois que y a une requete http qui arrive
+    //tu regarde le path , le 1er elemebnt du path est le nom du microService
+    //il envoie une rqt automatiquement vers discovery pr récuperer l 'adresse ip et le port
+    // et il retourne le résultat
+    @Bean
+    DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+            ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp)
+    {
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
+
+
 
 }
