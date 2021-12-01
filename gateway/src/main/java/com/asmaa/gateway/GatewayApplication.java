a37920bd59e1493598a4d73ffdbef012d03a0f33
asmaabouta <asmaabouta99@gmail.com>
2021-12-01 22:04:46 +0100

    commit

--- gateway/src/main/java/com/asmaa/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/asmaa/gateway/GatewayApplication.java
@@ -6,6 +6,17 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -23,6 +34,33 @@ public class GatewayApplication {
     {
         return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
     }
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                ServerHttpResponse response = ctx.getResponse();
+                HttpHeaders headers = response.getHeaders();
+                headers.add("Access-Control-Allow-Origin", "*");
+                headers.add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+                headers.add("Access-Control-Max-Age", "3600");
+                headers.add("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(ctx);
+        };
+    }
+   // @Bean
+   // RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+     //   return builder.routes()
+       //         .route(r->r.path("/cutomers/**").uri("http://localhost:8082/*"))
+         //       .route(r->r.path("/products/**").uri("http://localhost:8081/*"))
+           //     .route(r->r.path("/invoices/**").uri("http://localhost:8083/*"))
+             //   .build();
+    //}
 
 
 
